2c859fb11c51b9ddc58dfee8cc391fd6e71c8f3c
Rafael Moreira <rmoreira@demandbase.com>
2015-04-28 21:54:35 -0700

    my name is ralf

--- test.rb
+++ test.rb
@@ -2,3 +2,5 @@
 
 puts "Hello, my name is not Sam. My name is Bob"
 puts "_________________________________________"
+puts "Hello, my friend. My name is Ralf"
+puts "_________________________________________"
